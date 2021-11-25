afa915884f18a0c3783f8361643f1c1ba7a6cccc
Mahadi001 <sadat.himel@gmail.com>
2021-11-25 13:57:00 +0600

    finaly finised

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -41,34 +41,60 @@ const quizData = [
   },
 ];
 
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const quiz = document.getElementById("quiz");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
 
-
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
+loadQuiz();
 
-
-function loadQuiz(){
-    const currentQuizData = quizData[currentQuiz];
-    questionEl.innerHTML = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-    
+function loadQuiz() {
+    deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerHTML = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
-submitBtn.addEventListener("click", ()=>{
-    currentQuiz++;
-    if(currentQuiz < quizData.length){
-        loadQuiz();
-    }else {
-        alert("you finished! Get yourself an orange lemonade")
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
-})
+  });
+  return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl) => {
+    answerEl.checked = false
+});
+}
+
+submitBtn.addEventListener("click", () => {
+  // check to see the answer
+  const answer = getSelected();
+  if (answer) {
+      if(answer === quizData[currentQuiz].correct){
+        score++;
+      }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>Your answered correctly at ${score} / ${quizData.length} question.</h2>`
+    }
+  }
+});
